f188da37c65769992768400152d91482a8fe5f3c
Gary Vincent <gtvincent2000@gmail.com>
2026-06-22 15:58:55 -0500

    Added tray menu shell.

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,4 +1,7 @@
-use tauri::tray::TrayIconBuilder;
+use tauri::{
+    menu::MenuBuilder,
+    tray::TrayIconBuilder,
+};
 
 #[tauri::command]
 fn greet(name: &str) -> String {
@@ -10,9 +13,35 @@ pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
         .setup(|app| {
+            let menu = MenuBuilder::new(app)
+                .text("open_menu", "Open Menu")
+                .text("open_overlay", "Open Overlay")
+                .text("close_overlay", "Close Overlay")
+                .separator()
+                .text("quit", "Quit App")
+                .build()?;
+
             TrayIconBuilder::new()
                 .tooltip("Overlay Companion")
                 .icon(app.default_window_icon().unwrap().clone())
+                .menu(&menu)
+                .on_menu_event(|_app, event| {
+                    match event.id().as_ref() {
+                        "open_menu" => {
+                            println!("Open Menu clicked");
+                        }
+                        "open_overlay" => {
+                            println!("Open Overlay clicked");
+                        }
+                        "close_overlay" => {
+                            println!("Close Overlay clicked");
+                        }
+                        "quit" => {
+                            println!("Quit App clicked");
+                        }
+                        _ => {}
+                    }
+                })
                 .build(app)?;
 
             Ok(())
