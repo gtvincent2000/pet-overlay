f8c57c1eb2acf3c4c4d3c4c0d6abcc882b007224
Gary Vincent <gtvincent2000@gmail.com>
2026-06-22 15:49:33 -0500

    Added system tray icon.

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,4 +1,5 @@
-// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+use tauri::tray::TrayIconBuilder;
+
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
@@ -8,6 +9,14 @@ fn greet(name: &str) -> String {
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
+        .setup(|app| {
+            TrayIconBuilder::new()
+                .tooltip("Overlay Companion")
+                .icon(app.default_window_icon().unwrap().clone())
+                .build(app)?;
+
+            Ok(())
+        })
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
